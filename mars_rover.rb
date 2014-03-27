b3a12de1a386078c12da904bf1ecf7dcadfb075c
zap-rowsdower <brock.whitbread@gmail.com>
2014-03-27 14:24:58 -0400

    Changed Plateau class to a Commander class. Both classes ask for some input from the user to determine the position of the rover on the plane, it's orientation and the size of the plane.

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,12 +1,24 @@
-class Rover
-  
-  def initialize(x,y,d)
-    @x = x
-    @y = y
-    @d = d
+class Commander
+
+  def initialize
+    puts "Enter grid as x,y"
+    grid = gets.chomp
+    grid = grid.split(',')
+    @grid_x = grid[0]
+    @grid_y = grid[1]
   end
 
 end
 
-class Plateau
+class Rover
+  
+  def initialize(x,y,d)
+    puts "Enter Rover start position (x,y) and direction (d) as x,y,d:"
+    pos = gets.chomp
+    pos = pos.split(',')
+    @x = pos[0]
+    @y = pos[1]
+    @d = pos[2]
+  end
+
 end
