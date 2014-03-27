9ff690b4d7d64d1f5ae866e1255732bc8df47a55
zap-rowsdower <brock.whitbread@gmail.com>
2014-03-27 13:11:55 -0400

    Called my two methods on each class and printed the results. Assignment complete.

--- people.rb
+++ people.rb
@@ -27,6 +27,13 @@ end
 
 chris = Instructor.new("Chris")
 puts chris
+puts chris.teach
 
 christina = Student.new("Christina")
 puts christina
+puts christina.learn
+
+#     When the teach method is called on the student instance we get an "undefined method" 
+# error. This error occurs because the teach method is only available to objects that belong 
+# to the Instructor. If we had called learn on an object in the Instructor class we would 
+# see the same error for the same reasons.
